e2a77e50b42f209ec8bf4ce29c8d2849cb86ec53
UnderTheBridge <ramazanikbaev6@gmail.com>
2025-11-22 13:29:16 +0500

    Add some things :3

--- script.js
+++ script.js
@@ -14,6 +14,7 @@ function InputLetters(){
 InputLetters();
 addEventListener("keydown", (enter)=>{
     if(enter.key == 'Enter'){
+        if(CheckEmptyField()) return;
         row[currentLine].querySelectorAll(".cell").forEach(elem=>{
             elem.children[0].setAttribute("disabled","disabled")
         })
@@ -23,15 +24,35 @@ addEventListener("keydown", (enter)=>{
     }
 })
 
+
+function CheckEmptyField(){
+    let inputs = document.querySelectorAll("input");
+    for (let i = 0; i < inputs.length; i++) {
+        if(inputs[i].attributes.disabled) continue;
+        // console.log(inputs[i].value);
+        if(inputs[i].value == "") return true;
+    }
+    return false;
+}
+
+// addEventListener("keydown",(e)=>{
+//     if(e.key = "P"){
+//         CheckEmptyField();
+//     }
+// })
+
 function CheckAnswer(){
     let temp = row[currentLine].querySelectorAll(".cell");
     for (let i = 0; i < 5; i++) {
         if(Word[i] == temp[i].children[0].value){
-            temp[i].style.backgroundColor = "green";
+            temp[i].style.backgroundColor = "#0fb12a79";
+            temp[i].children[0].style.color = "#00ff006e";
         }else if(Word.includes(temp[i].children[0].value)){
-            temp[i].style.backgroundColor = "yellow";
+            temp[i].style.backgroundColor = "#b1ae0f79";
+            temp[i].children[0].style.color = "#eeff006e";
         }else{
-            temp[i].style.backgroundColor = "red";
+            temp[i].style.backgroundColor = "#b10f2d79";
+            temp[i].children[0].style.color = "#ff00006e";
         }
     }
     row[currentLine].querySelectorAll(".cell").forEach(elem=>{
